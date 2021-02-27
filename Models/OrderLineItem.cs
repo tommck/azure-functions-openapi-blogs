00aef802a7d9b6866bd7972d17ae75e4e56e112c
Tom McKearney <tommck@gmail.com>
2021-02-27 12:38:02 -0500

    Adding C# Comments to the open api doc

--- Models/OrderLineItem.cs
+++ Models/OrderLineItem.cs
@@ -3,11 +3,20 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Bmazon.Models
 {
+  /// <summary>
+  /// A single line item in an order
+  /// </summary>
   public class OrderLineItem
   {
+    /// <summary>
+    /// The number of the SKU desired
+    /// </summary>
     [Required, NotNull, Range(1, 1000)]
     public int Quantity { get; set; }
 
+    /// <summary>
+    /// The SKU identifying the item to purchase
+    /// </summary>
     [Required, NotNull, MinLength(1)]
     public string SKU { get; set; }
   }
